0c8b49fc02954c5a2918157831104879891de58a
Inayat <73791952+hafizip00@users.noreply.github.com>
2024-04-02 03:05:14 +0500

    Files Added
    All files Added

---
+++ LandingPage/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for LandingPage project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.contrib import admin
+from django.urls import path
+from SiteApp.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',mode1,name="home"),
+    path('home',mode1,name="home"),
+    path('about/',mode2,name="about"),
+    path('service/',mode3,name="service"),
+    path('team/',mode4,name="team"),
+    path('why/',mode5,name="why"),
+]
