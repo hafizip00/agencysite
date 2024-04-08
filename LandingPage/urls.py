063a28018248e8575d05242b6aa222dbb9bd375e
Inayat <73791952+hafizip00@users.noreply.github.com>
2024-04-08 11:52:12 +0500

    Update urls.py

--- LandingPage/urls.py
+++ LandingPage/urls.py
@@ -30,3 +30,6 @@ urlpatterns = [
     path('team/',mode4,name="team"),
     path('why/',mode5,name="why"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
